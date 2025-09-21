f41a2656ce9679f98bce3ebd10b0c5230524aaa3
Joe Strauss <cuppajava@tutanota.com>
2025-09-21 18:30:11 -0500

    Initialize Spring Boot and SvelteKit project structure

---
+++ phish-patrol/src/main/java/com/phish_patrol/PhishPatrolApplication.java
@@ -0,0 +1,13 @@
+package com.phish_patrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhishPatrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhishPatrolApplication.class, args);
+	}
+
+}
