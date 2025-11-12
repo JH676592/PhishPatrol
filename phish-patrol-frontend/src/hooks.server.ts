6b4007ce1b4fb1f628c7eac73f9f4b2748a3e3c7
Joe Strauss <cuppajava@tutanota.com>
2025-11-12 11:15:02 -0600

    Improve prompts

--- phish-patrol-frontend/src/hooks.server.ts
+++ phish-patrol-frontend/src/hooks.server.ts
@@ -3,6 +3,20 @@ import { GEMINI_API_KEY } from '$env/static/private';
 import { addScenario } from '$lib/server/scenarioStore';
 import type { Scenario } from "$lib/types";
 
+const general_prompt = `Generate a scenario for our game.
+
+Player’s Role: Cyber Security Agent
+You recently got a job with Phish Patrol Task Force (PPTF) - an international cyber security organization dedicated to combating digital deception. You are the lead analyst in the Simulation and Training Division where you are tested against live phishing replicas to identify a hacker syndicate.
+
+Mission / Storyline:
+PPTF has been monitoring a surge in coordinated phishing attacks by a hacker syndicate known as The Lure. To help protect users, you have been placed inside a desktop simulation that reproduces the attack patterns seen in the field. Analyze text messages and emails to determine if they are scams or not. Each message you correctly identify helps trace The Lure’s digital footprint and gets us one step closer to unveiling the true attacker’s real identities. Example of messages include bank login alert, package delivery failed, CEO/executive request, IT password reset, HR payroll form asking for personal details, job/recruiter, social media account verification DM, etc.
+
+Make sure the scenarios are challenging and nuanced. Make sure emails contains a subject line and a from line.\n
+
+Also ensure that newline characters, \\n, are used, especially in lists and the different fields of emails.
+`
+
+
 const prompts = [
   "Compose a short phishing email. The type is EMAIL. Additionally, provide a short explanation about what makes this email identifiable as a scam.",
   "Compose a short legitimate email. The type is EMAIL. Additionally, provide a short explanation about what makes this email legitimate as opposed to looking like a scam.",
@@ -24,7 +38,7 @@ async function generateAndStoreScenario() {
     
     const response = await ai.models.generateContent({
       model: "gemini-2.5-flash",
-      contents: prompts[randomIndex],
+      contents: general_prompt + prompts[randomIndex],
       config: {
         responseMimeType: "application/json",
         responseSchema: {
