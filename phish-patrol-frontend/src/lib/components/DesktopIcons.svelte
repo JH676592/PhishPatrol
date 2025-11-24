68e312046703e06d361ed9dd46c5071788c219bd
Joe Strauss <cuppajava@tutanota.com>
2025-11-24 09:33:30 -0600

    Revert "Merge branch 'dev' into feedback_ui"
    This reverts commit bbce95d8aa1c7f2e2020c0c05b7f2677952e4e76, reversing
    changes made to b814b2a22e40437da7d5193f636c89f0d4d7668c.

--- phish-patrol-frontend/src/lib/components/DesktopIcons.svelte
+++ phish-patrol-frontend/src/lib/components/DesktopIcons.svelte
@@ -2,38 +2,33 @@
   import { scenarioQueue } from '$lib/stores/scenarioQueue';
   import { ScenarioType } from '$lib/types';
 
+
   export let on: { openMessages: () => void; openEmail: () => void};
   
-  export let scenarioOpen: boolean;
-  export let disabled: boolean = false;
-
   // Variables to keep track of email and sms count for queue
   let emailCount = 0;
   let smsCount = 0;
-
+  
   // when scenarioQueue store changes it updates emailCount, same for smsCount
   $: emailCount = $scenarioQueue.filter(s => s.type === ScenarioType.EMAIL).length;
   $: smsCount = $scenarioQueue.filter(s => s.type === ScenarioType.SMS).length;
-
+  
   // Called when email icon clicked and opens msg window
   function handleClickMessages() {
     console.log("Messages icon clicked"); //for debugging
-    if (disabled || scenarioOpen || smsCount === 0) return;
     on?.openMessages?.();
   }
 
   // Called when sms icon clicked and opens msg window
   function handleClickEmail() {
     console.log("SMS icon clicked"); // debugging
-    if (disabled || scenarioOpen || emailCount === 0) return;
     on?.openEmail?.();
   }
-
-
+ 
 </script>
 
 <div class="icons">
-  <button class="icon" type="button" on:click={handleClickEmail} disabled={scenarioOpen || emailCount === 0}>
+  <button class="icon" type="button" on:click={handleClickEmail}>
     {#if emailCount > 0}
       <div class="badge">{emailCount}</div>
     {/if}
@@ -41,24 +36,16 @@
     <div class="icon-label">Email</div>
   </button>
   
-  <button class="icon" type="button" on:click={handleClickMessages} disabled={scenarioOpen || smsCount === 0}>
+  <button class="icon" type="button" on:click={handleClickMessages}>
     {#if smsCount > 0}
       <div class="badge">{smsCount}</div>
     {/if}
     <img src="/icons/chat.png" alt="SMS" class="icon-img"/>
     <div class="icon-label">Messages</div>
-
   </button>
 </div>
 
 <style>
-
-  .icon:disabled {
-    pointer-events: none;
-    opacity: 0.5;
-    filter: grayscale(70%);
-    cursor: none;
-  }
   
   .icons {
       position: absolute;
@@ -81,16 +68,11 @@
       border: none;
       cursor: pointer
   }
-  .icon-label {
-    font-size: .9rem;
-    font-weight: 400;
-    letter-spacing: .04rem;
-  }
 
   .icon-img {
       width: 64px;
       height: 64px;
-      margin-bottom: 4px;
+      margin-bottom: 8px;
   }
 
   .badge {
--- phish-patrol-frontend/src/routes/+page.svelte
+++ phish-patrol-frontend/src/routes/+page.svelte
@@ -8,21 +8,6 @@
   import { ScenarioType } from '$lib/types';
   import { get } from 'svelte/store';
 
-  let showMessages = false;
-  let currentScenario: Scenario | null = null;
-  let showLogin = true; 
-  let iconsDisabled = false;
-
-  // Opens next scenario for SMS from the queue from the store and displays it in MessageWindow
-  function openMessages() {
-    iconsDisabled = true;
-    const queue = get(scenarioQueue); //queue
-    const nextSMS = queue.find(s => s.type === ScenarioType.SMS); //first sms in queue
-    if (nextSMS) {
-      currentScenario = nextSMS; //set as current
-      showMessages = true; //show MessageWindow
-      removeScenarioFromQueue(nextSMS.id); //remove from queue
-    }
   
   function logout() {
     localStorage.removeItem('token');
@@ -30,95 +15,94 @@
     clearScenarioQueue();
   }
 
-  // Same as for SMS but Email
-  function openEmail() {
-    iconsDisabled = true;
-    const queue = get(scenarioQueue);
-    const nextEmail = queue.find(s => s.type === ScenarioType.EMAIL);
-    if (nextEmail) {
-      currentScenario = nextEmail;
-      showMessages = true;
-      removeScenarioFromQueue(nextEmail.id);
-    }
+let showMessages = false;
+let currentScenario: Scenario | null = null;
+
+// Opens next scenario for SMS from the queue from the store and displays it in MessageWindow
+function openMessages() {
+  const queue = get(scenarioQueue); //queue
+  const nextSMS = queue.find(s => s.type === ScenarioType.SMS); //first sms in queue
+  if (nextSMS) {
+    currentScenario = nextSMS; //set as current
+    showMessages = true; //show MessageWindow
+    removeScenarioFromQueue(nextSMS.id); //remove from queue
   }
+}
 
-  // handles closing window when user selects continue button
-  function handleComplete() {
-      showMessages = false; //hide MessageWindow
-      currentScenario = null; //clear current scenario
-      iconsDisabled = false;
+// Same as for SMS but Email
+function openEmail() {
+  const queue = get(scenarioQueue);
+  const nextEmail = queue.find(s => s.type === ScenarioType.EMAIL);
+  if (nextEmail) {
+    currentScenario = nextEmail;
+    showMessages = true;
+    removeScenarioFromQueue(nextEmail.id);
   }
+}
 
-  </script>
+// handles closing window when user selects continue button
+function handleComplete() {
+    showMessages = false; //hide MessageWindow
+    currentScenario = null; //clear current scenario
+}
 
-  <!-----------------------------DESKTOP/HOME PAGE----------------------------------------->
+</script>
 
-  <head>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@700&display=swap" rel="stylesheet">
-  </head>
+<!-----------------------------DESKTOP/HOME PAGE----------------------------------------->
 
-  <div class="desktop">
-    <header class="header">
-      <div class="header-content">
-        <h1>PHISH PATROL</h1>
-        <img src="/icons/fishing.png" alt="Fish" class="logo"/>
-        <!----<a href="https://www.flaticon.com/free-icons/fishing" title="fishing icons">Fishing icons created by Hilmy Abiyyu A. - Flaticon</a>-->
+<head>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@700&display=swap" rel="stylesheet">
+</head>
+
+<div class="desktop">
+  <header class="header">
+    <div class="header-content">
+      <h1>PHISH PATROL</h1>
+      <img src="/icons/fishing.png" alt="Fish" class="logo"/>
+      <!----<a href="https://www.flaticon.com/free-icons/fishing" title="fishing icons">Fishing icons created by Hilmy Abiyyu A. - Flaticon</a>-->
+    </div>
+  </header>
+
+  <!---------Desktop Icons------------>
+  <DesktopIcons on={{ openMessages: openMessages, openEmail: openEmail }}/>
+
+
+  <!---------Message Window------------>
+  {#if showMessages && currentScenario}
+      <MessageWindow {currentScenario} onComplete={handleComplete} />
+  {/if}
+
+
+  <!-----------------------------Taskbar---------------------------------->
+  <div class="taskbar">
+
+    <!-----------Left Items------------>
+    <div class="taskbar-left">
+      <div class="left-item">
+          <img src="/icons/play.png" alt="Play" class="left-icons"/>
+        <!-----<a href="https://www.flaticon.com/free-icons/play-button" title="play button icons">Play button icons created by Freepik - Flaticon</a>-->
+        <div class="left-label">Play</div>
       </div>
-    </header>
-
-    <!---------Desktop Icons------------>
-    <DesktopIcons
-      disabled={iconsDisabled}
-      scenarioOpen={showMessages}
-      on={{ openMessages, openEmail }}/>
-
-    <!---------Message Window------------>
-    {#if showMessages && currentScenario}
-        <MessageWindow {currentScenario} onComplete={handleComplete}/>
-    {/if}
-
-
-    <!-----------------------------Taskbar---------------------------------->
-    <div class="taskbar">
-
-      <!-----------Left Items------------>
-      <div class="taskbar-left">
-        <div class="left-item">
-            <img src="/icons/play.png" alt="Play" class="left-icons"/>
-          <!-----<a href="https://www.flaticon.com/free-icons/play-button" title="play button icons">Play button icons created by Freepik - Flaticon</a>-->
-          <div class="left-label">Play</div>
-        </div>
-        <div class="left-item">
-          <img src="/icons/book.png" alt="Resources" class="left-icons"/>
-          <div class="left-label">Resources</div>
-        </div>
-        <div class="left-item">
-            <img src="/icons/tutorial.png" alt="Tutorial" class="left-icons"/>
-          <!-----<a href="https://www.flaticon.com/free-icons/video-tutorial" title="video tutorial icons">Video tutorial icons created by Freepik - Flaticon</a>-->
-          <div class="left-label">Tutorial</div>
-        </div>
+      <div class="left-item">
+        <img src="/icons/book.png" alt="Resources" class="left-icons"/>
+        <div class="left-label">Resources</div>
       </div>
-
-      <!-----------Center Items-------------->
-      <div class="taskbar-center">
-        <div class="search-container">
-            <input type="text" class="search-input" placeholder="Search database..."/>
-        </div>
+      <div class="left-item">
+          <img src="/icons/tutorial.png" alt="Tutorial" class="left-icons"/>
+        <!-----<a href="https://www.flaticon.com/free-icons/video-tutorial" title="video tutorial icons">Video tutorial icons created by Freepik - Flaticon</a>-->
+        <div class="left-label">Tutorial</div>
       </div>
+    </div>
 
-      <!-----------Right Items------------->
-      <div class="taskbar-right">
-        <div class="wifi">
-          <img src="/icons/wifi.png" alt="Wifi" class="right-icons">
-          <!---<a href="https://www.flaticon.com/free-icons/wifi" title="wifi icons">Wifi icons created by Aldo Cervantes - Flaticon</a>-->
-          </div>
-        <div class="battery">
-          <img src="/icons/battery.png" alt="Battery" class="right-icons"/>
-          <!--<a href="https://www.flaticon.com/free-icons/battery" title="battery icons">Battery icons created by Stockio - Flaticon</a>-->
-        </div>
-        <div class="datetime">
-          <div class="clock">11:52 AM</div>
-          <div class="date">10/31/2025</div>
+    <!-----------Center Items-------------->
+    <div class="taskbar-center">
+      <div class="search-container">
+          <input type="text" class="search-input" placeholder="Search database..."/>
+      </div>
+    </div>
+
+    <!-----------Right Items------------->
+    <div class="taskbar-right">
       <div>
         {#if $tokenStore}
         <button class="logout-btn" on:click={logout}>Logout</button>
@@ -127,182 +111,212 @@
       <div class="wifi">
         <img src="/icons/wifi.png" alt="Wifi" class="right-icons">
         <!---<a href="https://www.flaticon.com/free-icons/wifi" title="wifi icons">Wifi icons created by Aldo Cervantes - Flaticon</a>-->
-       </div>
+        </div>
+      <div class="battery">
+        <img src="/icons/battery.png" alt="Battery" class="right-icons"/>
+        <!--<a href="https://www.flaticon.com/free-icons/battery" title="battery icons">Battery icons created by Stockio - Flaticon</a>-->
+      </div>
+      <div class="datetime">
+        <div class="clock">11:52 AM</div>
+        <div class="date">10/31/2025</div>
+      </div>
     </div>
-    <LoginModal />
   </div>
-  <!--Login Modal on site launch-->
+  <LoginModal />
+</div>
+<!--Login Modal on site launch-->
 
-        
+      
 
-  <!-----------------------------------STYLES------------------------------------->
-  <style>
+<!-----------------------------------STYLES------------------------------------->
+<style>
 
-    :global(html,body) {
-      margin: 0;
-      padding: 0;
-      height: 100%;
-      overflow: hidden;
-    }
+  :global(html,body) {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    overflow: hidden;
+  }
 
-    .desktop {
-      position: relative;
-      width: 100vw;
-      height: 100vh;
-      background: url('/background/desktop.png') no-repeat center center;
-      background-size: cover;
-      display: flex;
-      flex-direction: column;
-      justify-content: space-between;
-      font-family: 'Segoe UI', sans-serif;
-      color: white;
-      flex-wrap: wrap;
-    }
+  .desktop {
+    position: relative;
+    width: 100vw;
+    height: 100vh;
+    background: url('/background/desktop.png') no-repeat center center;
+    background-size: cover;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    font-family: 'Segoe UI', sans-serif;
+    color: white;
+    flex-wrap: wrap;
+  }
 
-    .header-content {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      gap: 12px;
-    }
+  .header-content {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 12px;
+  }
 
-    header h1 {
-      font-family: 'Roboto Mono';
-      font-style: normal;
-      font-weight: 700;
-      font-size: 48px;
-      line-height: 140%;
-      text-align: center;
-      letter-spacing: 0.15em;
-      color: #000000;
-      mix-blend-mode: hard-light;
-      opacity: 0.8;
-    }
+  header h1 {
+    font-family: 'Roboto Mono';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 48px;
+    line-height: 140%;
+    text-align: center;
+    letter-spacing: 0.15em;
+    color: #000000;
+    mix-blend-mode: hard-light;
+    opacity: 0.8;
+  }
 
-    .logo {
-      height: 65px;
-      width: 65px;
-      line-height: 140%;
-      text-align: center;
+  .logo {
+    height: 65px;
+    width: 65px;
+    line-height: 140%;
+    text-align: center;
 
-    }
+  }
 
-    /* Taskbar */
-    .taskbar {
-      display: flex;
-      flex-wrap: nowrap;
-      justify-content: space-between;
-      align-items: center;
-      overflow: hidden;
-      min-width: 0;
-      height: 78px;
-      padding: 0 16px;
-      background: #b9b2b29e;
-      flex-wrap: wrap;
-    }
+  /* Taskbar */
 
-    .taskbar-center, .taskbar-left, .taskbar-right {
-      display: flex;
-      align-items: center;
-      gap: 20px;
-      flex: 1 1 auto;
-      min-width: 0;
-    }
+  .taskbar {
+    display: flex;
+    flex-wrap: nowrap;
+    justify-content: space-between;
+    align-items: center;
+    overflow: hidden;
+    min-width: 0;
+    height: 78px;
+    padding: 0 16px;
+    background: #b9b2b29e;
+    flex-wrap: wrap;
+  }
 
-    .taskbar-center {
-      justify-content: center;
-      flex: 0 1 300px;
-    }
+  .taskbar-center, .taskbar-left, .taskbar-right {
+    display: flex;
+    align-items: center;
+    gap: 20px;
+    flex: 1 1 auto;
+    min-width: 0;
+  }
 
-    .taskbar-right {
-      justify-content: flex-end;
-    }
-    
-    /* Taskbar Left */
-    .taskbar-left {
-      display: flex;
-      gap: 40px;
-      padding: 10px;
-      flex-wrap: wrap;
-    }
+  .taskbar-center {
+    justify-content: center;
+    flex: 0 1 300px;
+  }
 
-    .left-item {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      color: white;
-      cursor: pointer;
-      user-select: none;
-      font-size: 12px;
-    }
+  .taskbar-right {
+    justify-content: flex-end;
+  }
+  
+  /* Taskbar Left */
 
-    .left-icons {
-      width: 50px;
-      height: 50px;
-      filter: drop-shadow(2px 2px 4px rgba(0,0,0,0.6));
-    }
+  .taskbar-left {
+    display: flex;
+    gap: 40px;
+    padding: 10px;
+    flex-wrap: wrap;
+  }
 
-    .left-label {
-      margin-top: 2px;
-      font-size: 14px;
-      font-weight: 800;
-      text-align: center;  
-      letter-spacing: 1px;
-      text-shadow:   
-      -1px -1px 0 #2a2699d1,
-      1px -1px 0 #2a2699d1,
-      -1px  1px 0 #2a2699d1,
-      1px  1px 0 #2a2699d1;
-    }
+  .left-item {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    color: white;
+    cursor: pointer;
+    user-select: none;
+    font-size: 12px;
+  }
+
+  .left-icons {
+    width: 50px;
+    height: 50px;
+    filter: drop-shadow(2px 2px 4px rgba(0,0,0,0.6));
+  }
+
+  .left-label {
+    margin-top: 2px;
+    font-size: 14px;
+    font-weight: 800;
+    text-align: center;  
+    letter-spacing: 1px;
+    text-shadow:   
+    -1px -1px 0 #2a2699d1,
+     1px -1px 0 #2a2699d1,
+    -1px  1px 0 #2a2699d1,
+     1px  1px 0 #2a2699d1;
+  }
 
 
-    /* Search Bar */
-    .search-container {
-      display: flex;
-      flex: 1;
-      justify-content: center;
-      padding: 0 12px;
-      flex-wrap: wrap;
-    }
+  /* Search Bar */
 
-    .search-input {
-      padding: 6px 10px;
-      font-size: 15px;
-      border-radius: 6px;
-      background-color: #e0e0e0;
-      width: 100%;
-      max-width: 250%;
-      box-shadow: inset 1px 1px 2px 2px rgba(3, 219, 89, 0.426);
-    }
+  .search-container {
+    display: flex;
+    flex: 1;
+    justify-content: center;
+    padding: 0 12px;
+    flex-wrap: wrap;
+  }
 
-    /* Taskbar Right */
-    .taskbar-right {
-      display: flex;
-      align-items: center;
-      padding: 5px;
-    }
+  .search-input {
+    padding: 6px 10px;
+    font-size: 15px;
+    border-radius: 6px;
+    background-color: #e0e0e0;
+    width: 100%;
+    max-width: 250%;
+    box-shadow: inset 1px 1px 2px 2px rgba(3, 219, 89, 0.426);
+  }
 
-    .right-icons {
-      width: 30px;
-      height: 30px;
-      object-fit: contain;
-      padding: 12px;
-    }
+  /* Logout Button */
+  .logout-btn {
+    background-color: #ef4444;
+    color: white;
+    font-family: 'Roboto Mono', monospace;
+    padding: 0.5rem 1rem;
+    border-radius: 8px;
+    font-size: 0.9rem;
+    font-weight: bold;
+    cursor: pointer;
+    margin-left: 12px;
+  }
 
-    .datetime {
-      display: flex;
-      padding: 10px;
-      flex-direction: column;
-      align-items: flex-end;
-      gap: 8px;
-      color: rgb(0, 0, 0);
-      font-size: 14px;
-      font-weight: 700;
-      letter-spacing: 1.1px;
-      text-shadow:   
-      .5px 0px 0 rgba(255, 255, 255, 0.653),
-      0px 0px 0 rgba(255, 255, 255, 0.653),
-      0px  0px 0 rgba(255, 255, 255, 0.653),
-      0px  0px 0 rgba(255, 255, 255, 0.653)
-    }
-  </style>
+  .logout-btn:hover {
+    background-color: #dc2626;
+  }
+
+  /* Taskbar Right */
+  
+  .taskbar-right {
+    display: flex;
+    align-items: center;
+    padding: 5px;
+  }
+
+  .right-icons {
+    width: 30px;
+    height: 30px;
+    object-fit: contain;
+    padding: 12px;
+  }
+
+  .datetime {
+    display: flex;
+    padding: 10px;
+    flex-direction: column;
+    align-items: flex-end;
+    gap: 8px;
+    color: rgb(0, 0, 0);
+    font-size: 14px;
+    font-weight: 700;
+    letter-spacing: 1.1px;
+    text-shadow:   
+    .5px 0px 0 rgba(255, 255, 255, 0.653),
+     0px 0px 0 rgba(255, 255, 255, 0.653),
+    0px  0px 0 rgba(255, 255, 255, 0.653),
+     0px  0px 0 rgba(255, 255, 255, 0.653)
+  }
+</style>
